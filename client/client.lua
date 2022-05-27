89415cd0b4942fa98846e896f9962e3006462026
sTREXiia <105889822+sTREXiia@users.noreply.github.com>
2022-05-27 19:58:06 +0430

    revert

--- client/client.lua
+++ client/client.lua
@@ -85,7 +85,7 @@ RegisterNetEvent('qb-clothes:clothingShop', function()
 	exports['qb-menu']:openMenu({
         {
             header = "👚 | Clothing Store Options",
-            isMenuHeader = false, -- Set to true to make a nonclickable title
+            isMenuHeader = true, -- Set to true to make a nonclickable title
         },
         {
             header = "Buy Clothing",
